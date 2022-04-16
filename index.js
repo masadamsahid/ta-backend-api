6ed278c50b1f80403999254b67bf20825653c0b9
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-04-16 15:01:54 +0700

    apollo server setup and connect to mongodb

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello, masadamsahid! \nLet's start building the API so you can graduate faster \nYour mom and dad crying on your tution fee :')")
+import {ApolloServer, gql} from 'apollo-server'
+import mongoose from "mongoose"
+import  dotenv from 'dotenv'
+
+// import typeDefs from './graphql/typeDefs.js'
+// import resolvers from './graphql/resolvers/index.js'
+
+const typeDefs = gql`
+  type Query{
+      sayHi: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    sayHi: () => "Hello HI!"
+  }
+}
+
+dotenv.config()
+const PORT = process.env.PORT || 5000
+const MONGODB = process.env.MONGODB
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req })
+})
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+  .then(()=>{
+    console.log("MONNGODB Connected! Yay 😊🙌")
+    return server.listen({ port: PORT})
+  })
+  .then((res) => {
+  console.log(`Server running at ${res.url}`)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
