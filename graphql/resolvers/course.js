8d6a434c05eea8b789ee1e1dbc84a75df3050890
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-04-18 18:19:20 +0700

    createCourse and save it to mongodb

--- graphql/resolvers/course.js
+++ graphql/resolvers/course.js
@@ -1,4 +1,5 @@
 import checkAuth from "../../utils/checkAuth.js";
+import Course from "../../models/Course.js  ";
 
 
 const courseResolvers = {
@@ -6,13 +7,12 @@ const courseResolvers = {
     async createCourse(proxy , {courseCode, title, tutor, description, price}, context) {
 
       /*TODO:
-       * 1. Check loginAuth
+       * 1. Check loginAuth (NICE! COMPLETED ✔)
        * 2. Throw an Error if not logged in
        * 3. Check User's role
        * 4. Throw an Error if the role is not admin or tutor
        */
       const user = checkAuth(context)
-      console.log(user)
 
       /*TODO:
        * 1. Check Course with courseCode already exists or not
@@ -23,9 +23,23 @@ const courseResolvers = {
        * 1. Create a new Course
        * 2. Save the new Course
        */
+      const course = new Course({
+        courseCode,
+        title,
+        tutor,
+        description,
+        price,
+        salesCount: 0,
+        createdAt: new Date().toISOString()
+      })
+      const res = await course.save()
 
       /*TODO: Return response
        */
+      return {
+        ...res._doc,
+        id: res._id,
+      }
 
     }
   }
